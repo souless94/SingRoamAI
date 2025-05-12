ec96148e5a002225f83c029e43ed6f1347802560
souless94 <clwk94@gmail.com>
2025-05-12 12:18:00 +0800

    Add home button in login page

--- app/auth/[[...path]]/page.tsx
+++ app/auth/[[...path]]/page.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { redirectToAuth } from 'supertokens-auth-react';
-import SuperTokens from 'supertokens-auth-react/ui';
-import { ThirdPartyPreBuiltUI } from 'supertokens-auth-react/recipe/thirdparty/prebuiltui';
+import { useEffect, useState } from "react";
+import { redirectToAuth } from "supertokens-auth-react";
+import SuperTokens from "supertokens-auth-react/ui";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Auth() {
   // if the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    if (
-      SuperTokens.canHandleRoute([ThirdPartyPreBuiltUI]) === false
-    ) {
+    if (SuperTokens.canHandleRoute([ThirdPartyPreBuiltUI]) === false) {
       redirectToAuth({ redirectBack: false });
     } else {
       setLoaded(true);
@@ -19,7 +19,22 @@ export default function Auth() {
   }, []);
 
   if (loaded) {
-    return SuperTokens.getRoutingComponent([ThirdPartyPreBuiltUI]);
+    return (
+      <div className="flex min-h-screen flex-col overflow-hidden">
+        <section>
+          {SuperTokens.getRoutingComponent([ThirdPartyPreBuiltUI])}
+        </section>
+        <section>
+          <div className="flex items-center">
+            <div className="mx-auto">
+              <Button asChild>
+                <Link href="/">Back to Home</Link>
+              </Button>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 
   return null;
