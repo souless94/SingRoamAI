2ecd944ff7d849e5b7394b892f51f785606c7fd3
souless94 <clwk94@gmail.com>
2025-08-18 23:45:50 +0800

    add alert for google login

--- app/auth/[[...path]]/page.tsx
+++ app/auth/[[...path]]/page.tsx
@@ -6,6 +6,8 @@ import SuperTokens from "supertokens-auth-react/ui";
 import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 
 export default function Auth() {
   // if the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
@@ -22,6 +24,11 @@ export default function Auth() {
     return (
       <div className="flex min-h-screen flex-col overflow-hidden">
         <section>
+          <Alert variant="default">
+            <Terminal />
+            <AlertTitle>Note</AlertTitle>
+            <AlertDescription>The Google Login is Demo</AlertDescription>
+          </Alert>
           {SuperTokens.getRoutingComponent([ThirdPartyPreBuiltUI])}
         </section>
         <section>
